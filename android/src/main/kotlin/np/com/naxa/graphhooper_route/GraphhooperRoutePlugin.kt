cf65cad9460b1bc344063156cd921cc70b5e07f3
igaurab <sushuoit@gmail.com>
2019-11-05 16:58:57 +0545

    WIP Implemented unzip function and minor changes

--- android/src/main/kotlin/np/com/naxa/graphhooper_route/GraphhooperRoutePlugin.kt
+++ android/src/main/kotlin/np/com/naxa/graphhooper_route/GraphhooperRoutePlugin.kt
@@ -1,10 +1,17 @@
 package np.com.naxa.graphhooper_route
 
 import android.annotation.SuppressLint
+import android.app.DownloadManager
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.content.IntentFilter
 import android.location.LocationManager
 import android.os.AsyncTask
 import android.os.Build
 import android.os.Environment
+import android.util.Log
+import androidx.localbroadcastmanager.content.LocalBroadcastManager
 import com.google.gson.GsonBuilder
 import com.graphhopper.GHRequest
 import com.graphhopper.GraphHopper
@@ -14,19 +21,30 @@ import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+import io.flutter.plugin.common.PluginRegistry
 import io.flutter.plugin.common.PluginRegistry.Registrar
+import java.io.BufferedInputStream
 import java.io.File
 import java.lang.Exception
+import java.util.*
+import java.util.zip.ZipFile
 
-class GraphhooperRoutePlugin : MethodCallHandler {
+class GraphhooperRoutePlugin : MethodCallHandler, PluginRegistry.ActivityResultListener {
+
+    override fun onActivityResult(p0: Int, p1: Int, p2: Intent?): Boolean {
+        return true
+    }
 
     companion object {
         private lateinit var locationManager: LocationManager
-
+        private lateinit var ctx: Context
+        private lateinit var mpoints: List<Double>
         @JvmStatic
         fun registerWith(registrar: Registrar) {
             val channel = MethodChannel(registrar.messenger(), "graphhooper_route")
             channel.setMethodCallHandler(GraphhooperRoutePlugin())
+            ctx = registrar.activeContext()
+            LocalBroadcastManager.getInstance(ctx).registerReceiver(CalcPath().onDownloadComplete, IntentFilter(DownloadManager.ACTION_DOWNLOAD_COMPLETE))
         }
     }
 
@@ -36,6 +54,9 @@ class GraphhooperRoutePlugin : MethodCallHandler {
         } else if (call.method.equals("getRoutes")) {
             val mapPath = call.argument<String>("path_to_map")
             val points = call.argument<List<Double>>("points")
+            if (points != null) {
+                mpoints = points.toMutableList()
+            }
             CalcPath(result, mapPath).execute(points?.get(0), points?.get(1), points?.get(2), points?.get(3))
         } else {
             result.notImplemented()
@@ -49,6 +70,7 @@ class GraphhooperRoutePlugin : MethodCallHandler {
 
         var result: Result? = null
         var mapPath: String? = null
+        var downloadId: Long? = null
 
         constructor(result: Result, mapPath: String?) : this() {
             this.result = result
@@ -56,17 +78,62 @@ class GraphhooperRoutePlugin : MethodCallHandler {
         }
 
         private fun ifMapFileExists(): Boolean {
-            return getMapFolder().exists()
+            Log.d("IFMAPFILEEXISTS: ", File(Environment.getExternalStorageDirectory(), "/Download/nepal-gh/").exists().toString())
+            return File(Environment.getExternalStorageDirectory(), "/Download/nepal-gh").exists()
         }
 
-        private fun downloadMapFromRemoteSource(): String {
-            return "Map file is not found"
+        private fun downloadMapFromRemoteSource() {
+//            val baseUrl = ""
+//            val fileName = "nepal-gh.zip"
+//            val url = baseUrl
+//
+//            val request = DownloadManager.Request(Uri.parse(url))
+//            request.setAllowedNetworkTypes(DownloadManager.Request.NETWORK_MOBILE or DownloadManager.Request.NETWORK_WIFI)
+//            request.setTitle("Downloading $fileName")
+//            request.setDescription("Please wait")
+//            request.allowScanningByMediaScanner()
+//            request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED)
+//            request.setDestinationInExternalPublicDir(Environment.DIRECTORY_DOWNLOADS,"nepal-gh")
+//
+//            val manager = ctx.applicationContext.getSystemService(Context.DOWNLOAD_SERVICE) as DownloadManager
+//            downloadId = manager.enqueue(request)
+            unzip(Environment.getExternalStorageDirectory().absolutePath + "/Download/nepal-gh.zip", Environment.getExternalStorageDirectory().absolutePath + "/Download/")
+        }
+
+        val onDownloadComplete = object : BroadcastReceiver() {
+            override fun onReceive(contxt: Context?, intent: Intent?) {
+                Log.d("download: ", "DOWNLOAD COMPLETED")
+                unzip(Environment.getExternalStorageDirectory().absolutePath + "/Download/nepal-gh.zip", Environment.getExternalStorageDirectory().absolutePath + "/Download/")
+            }
+        }
+
+        private fun unzip(zipFile: String, targetPath: String) {
+            Log.d("UNZIP", targetPath)
+            Log.d("UNZIP", zipFile)
+
+            val zip = ZipFile(zipFile)
+            val enumeration = zip.entries()
+            while (enumeration.hasMoreElements()) {
+                val entry = enumeration.nextElement()
+                val destFilePath = File(targetPath, entry.name)
+                destFilePath.parentFile.mkdirs()
+                if (entry.isDirectory)
+                    continue
+                val bufferedIs = BufferedInputStream(zip.getInputStream(entry))
+                bufferedIs.use {
+                    destFilePath.outputStream().buffered(1024).use { bos ->
+                        bufferedIs.copyTo(bos)
+                    }
+                }
+            }
+            Log.d("MPOINTS: ", mpoints.toString())
+            CalcPath().doInBackground(mpoints.get(0), mpoints.get(1), mpoints.get(2), mpoints.get(3))
         }
 
         override fun doInBackground(vararg params: Double?): String? {
 
             try {
-                if(ifMapFileExists()) {
+                if (ifMapFileExists()) {
                     val sw = StopWatch().start()
                     val hopper = GraphHopper().forMobile()
                     hopper.load(File(getMapFolder(), "nepal").getAbsolutePath() + "-gh")
@@ -79,14 +146,16 @@ class GraphhooperRoutePlugin : MethodCallHandler {
                     time = sw.stop().seconds
                     val gson = GsonBuilder().serializeSpecialFloatingPointValues().create()
                     return resp.all.toString()
-                }else{
-                    return  downloadMapFromRemoteSource()
+                } else {
+                    Log.d("Download: ", "calling downloadFromMapSource")
+                    downloadMapFromRemoteSource()
+                    return "Map Downloaded"
                 }
             } catch (ex: Exception) {
                 ex.printStackTrace()
             }
 
-            return "Can't Load Map"
+            return "Cannot Load Map"
         }
 
         override fun onPreExecute() {
@@ -99,6 +168,7 @@ class GraphhooperRoutePlugin : MethodCallHandler {
             result?.success(path)
         }
 
+        @SuppressLint("ObsoleteSdkInt")
         fun getMapFolder(): File {
             result
             val mapsFolder: File
